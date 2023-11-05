1c608c277d9f1d89009d550656b5f1fbfaeb0950
XavierMurillo <xaviermurillosanchez@gmail.com>
2023-11-05 10:36:00 -0400

    changed colaborador search

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Buscador
         setColaboradores={setColaboradores}
-        colaboradores={colaboradores}
+        colaboradores={BaseColaboradores}
       />
       <Listado colaboradores={colaboradores} />
       <Formulario />
