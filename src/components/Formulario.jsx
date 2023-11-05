3c4f072f03420b6d5fefe9c2840fbd07c8cad5fb
Maur1998 <mmurillosanchez1998@gmail.com>
2023-11-05 10:25:02 -0400

    Mejorando formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,27 +1,54 @@
 import { useState } from "react"
 
 export const Formulario = () => {
-  const [nombre,setNombre] = useState("");
-  const [email,setEmail] = useState("");
-  const [edad,setEdad] = useState("");
-  const [cargo,setCargo] = useState("");
-  const [telefono, setTelefono] = useState("");
+
+  const [formulario, setFormulario] = useState({
+    nombre:'',
+    email:'',
+    edad:'',
+    cargo:'',
+    telefono:''
+  })
 
   const handleSubmit = () =>{
     event.preventDefault();
-    if(nombre=="" || email=="" || edad=="" || cargo=="" || telefono==""){
-      alert("Completa todos los campos")
+    let values = Object.values(formulario);
+    let flag = 0
+    values.forEach((value)=> {
+      if(value.length < 1){
+        flag = 1;
+      }else {
+        flag = 0
+      }
+    })
+    if(flag ==1){
+      alert("Completa todos los datos!")
     }
-  }
+    else{
+
+    }
+  };
+
+  const handleOnChange = (event) =>{
+    event.preventDefault();
+    const nombreinput = event.target.name;
+    const valorinput = event.target.value;
+
+    setFormulario({
+      ...formulario,
+      [nombreinput]:valorinput,
+    });
+  };
+
   return (
     <div>
       <h1>Agregar colaborador</h1>
-      <form onSubmit={handleSubmit}>
-        <input placeholder="Nombre del colaborador" name="nombre" id="nombre" onChange={(e)=>setNombre(e.target.value)} />
-        <input placeholder="Email del colaborador" name="email" id="email" onChange={(e)=>setEmail(e.target.value)} />
-        <input placeholder="Edad del colaborador" name="edad" id="edad" onChange={(e)=>setEdad(e.target.value)} />
-        <input placeholder="Cargo del colaborador" name="cargo" id="cargo" onChange={(e)=>setCargo(e.target.value)} />
-        <input placeholder="Teléfono del colaborador" name="telefono" id="telefono" onChange={(e)=>setTelefono(e.target.value)} />
+      <form className="form" onSubmit={handleSubmit}>
+        <input placeholder="Nombre del colaborador" name="nombre" id="nombre" value={formulario.nombre} onChange={handleOnChange} />
+        <input placeholder="Email del colaborador" name="email" id="email" value={formulario.email} onChange={handleOnChange} />
+        <input placeholder="Edad del colaborador" name="edad" id="edad" value={formulario.edad} onChange={handleOnChange} />
+        <input placeholder="Cargo del colaborador" name="cargo" id="cargo" value={formulario.cargo} onChange={handleOnChange} />
+        <input placeholder="Teléfono del colaborador" name="telefono" id="telefono" value={formulario.telefono} onChange={handleOnChange} />
         <button>Agregar colaborador</button>
       </form>
     </div>
